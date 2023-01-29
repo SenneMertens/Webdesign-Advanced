80dab54b748d5160db3f8724b2450754f13b08d2
Senne Mertens <r0758393@student.thomasmore.be>
2023-01-29 09:31:11 +0100

    Alert, Prompt, Confirm Update

---
+++ JavaScript/The JavaScript Language/JavaScript Fundamentals/Alert, Prompt, Confirm/Exercise 1/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let firstName, lastName;
+
+firstName = prompt(`What's your first name?`, `First name`);
+lastName = prompt(`What's your last name?`, `Last name`);
+
+alert(`Your name is ${firstName} ${lastName}.`);
