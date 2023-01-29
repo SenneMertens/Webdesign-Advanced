753465e8bccc2982d30cd887a8db057175a12d9d
Senne Mertens <r0758393@student.thomasmore.be>
2023-01-29 09:20:12 +0100

    Variables Update

---
+++ JavaScript/The JavaScript Language/JavaScript Fundamentals/Variables/Exercise 1/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let admin, firstName;
+
+firstName = `John`;
+admin = firstName;
+
+alert(`${admin}`);
---
+++ JavaScript/The JavaScript Language/JavaScript Fundamentals/Variables/Exercise 3/js/script.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const BIRTHDAY = `03.06.1988`;
+const age = 34;
